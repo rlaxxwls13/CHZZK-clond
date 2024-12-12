5550e90dbb783c33986c100b0a6fb53ccc690e0b
hajin <kimhajin01@naver.com>
2024-12-12 22:56:42 +0900

    Feat: 프로젝트 초기 생성
    2024.12.12

---
+++ src/main/java/yhchippo/chzzk_clone/ChzzkCloneApplication.java
@@ -0,0 +1,13 @@
+package yhchippo.chzzk_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChzzkCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChzzkCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yhchippo/chzzk_clone/ChzzkCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package yhchippo.chzzk_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChzzkCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
